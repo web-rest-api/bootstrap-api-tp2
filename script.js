0ba88cb6658fadaa0fc3827f3f6438ebf679cd8c
lostmart <martinpedraza1979@hmail.com>
2023-12-14 09:59:24 +0100

    fetch fn

--- script.js
+++ script.js
@@ -4,4 +4,22 @@ const BASE_URL = "https://basic-rest-flask.martinpedraza.repl.co"
 const url = BASE_URL + "/api/books"
 fetch(url)
 	.then((response) => response.json())
-	.then((json) => console.log(json))
+	.then((livres) => {
+		livres.forEach((livre) => {
+			document.querySelector(".row").innerHTML += `
+                <div class="col">
+                    <article class="card">
+                        <img src="${livre.imageUrl}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${livre.title}</h5>
+                           
+                              <button data-bs-toggle="modal"
+			                    data-bs-target="#bookModal" 
+                                class="btn btn-dark edit">Edit</button>
+                        </div>
+                    </article>
+                </div>
+            `
+			console.log(livre)
+		})
+	})
